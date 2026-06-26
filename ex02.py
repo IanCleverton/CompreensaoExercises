7579ff0e23b5fab47825e3fb5affaadd7db3bea6
IanCleverton <iancleverton106@gmail.com>
2026-06-26 01:31:09 -0300

    adiciona ex02

--- ex02.py
+++ ex02.py
@@ -1,3 +1,20 @@
 def descendentes(a):
     return [ nm for nm, _ in a[1] ] + \
            [ nm for f in a[1] for nm in descendentes(f) ]
+
+arv = ( "Maria", [ ("Joana", [ ("Lucio", []),
+                               ("Jõao", [])
+                             ]
+                   ),
+                   ("Pedro",[]),
+                   ("Patricia", [ ("Marina", [ ("Marcelo", []),
+                                               ("Tomás", [])
+                                             ]
+                                  )
+                                ]
+                   ),
+                   ("Marcos",[])
+                 ]
+      )
+
+descendentes(arv)
